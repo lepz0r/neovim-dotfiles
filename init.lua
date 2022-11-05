b643c7866d6a396cefdc708075bf678c8614151c
lepz0r <punkofthedeath@gmail.com>
2022-11-05 16:20:17 +0700

    Moved markdown-preview.nvim's config to it's own file, added lualine's config file

--- init.lua
+++ init.lua
@@ -1,25 +1,24 @@
 vim.cmd([[set nocp]])
 --vim.g.loaded_netrw = 1
 --vim.g.loaded_netrwPlugin = 1
-vim.g.instant_markdown_autostart=0
-vim.env.QT_QPA_PLATFORM=''
-vim.g.mkdp_browser='qutebrowser'
-vim.g.suda_smart_edit = 1
 vim.opt.mouse='a'
 vim.opt.clipboard='unnamedplus'
 vim.wo.number = true
 
 require('keybindings')
 require('plugins')
+
+require('markdown-preview-config.base')
+require('suda-config.base')
 --require('everforest.base')
-require('dracula.base')
+require('dracula-config.base')
 require('lsp')
 require('dashboard-config.base')
 require('telescope-config.base')
 --require('neovim-session-manager-config.base')
 require('nvim-tree-config.base')
 require("neovide-config.base")
-require('lualine').setup()
+require('lualine-config.base')
 --require('barbar-config.base')
 
 require'colorizer'.setup({
---
+++ lua/markdown-preview-config/base.lua
@@ -0,0 +1,3 @@
+vim.g.instant_markdown_autostart=0
+vim.env.QT_QPA_PLATFORM='wayland-egl'
+vim.g.mkdp_browser='qutebrowser'
