78ce3a621e2335c7e0a7680d30c2348cef33aa77
lepz0r <punkofthedeath@gmail.com>
2022-11-14 21:50:05 +0700

    Restructure directories

--- init.lua
+++ init.lua
@@ -5,24 +5,17 @@ vim.opt.mouse='a'
 vim.opt.clipboard='unnamedplus'
 vim.wo.number = true
 
-require('keybindings')
-require('plugins')
+require('core.keybindings')
+require('core.plugins')
 
 require('markdown-preview-config.base')
 require('suda-config.base')
---require('everforest.base')
 require('dracula-config.base')
 require('lsp')
 require('dashboard-config.base')
 require('telescope-config.base')
---require('neovim-session-manager-config.base')
 require('nvim-tree-config.base')
 require("neovide-config.base")
 require('lualine-config.base')
---require('barbar-config.base')
-
---require'colorizer'.setup({
---	'css';
---})
-require('neovim-gtk-options')
+require('core.gui')
 
